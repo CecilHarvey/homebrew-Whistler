636db55a8cab9f983e03dda920aa8974f058949e
Wei Mingzhi <weimingzhi@baidu.com>
2018-09-26 13:44:34 +0800

    fix

--- Formula/brpc.rb
+++ Formula/brpc.rb
@@ -1,11 +1,13 @@
 class Brpc < Formula
   desc "Industrial-grade RPC framework used throughout Baidu, with 1,000,000+ instances and thousands kinds of services."
   homepage "https://github.com/brpc/brpc"
-  url "https://github.com/brpc/brpc/archive/a9e954879fda1c63f0f772b47d87943129e5af1f.zip"
-  sha256 "0d2760022811664ad473bb7f11709116fd32d89330c49ac263b034e8866b1902"
+  url "https://github.com/brpc/brpc/archive/a9e954879fda1c63f0f772b47d87943129e5af1f.tar.gz"
+  sha256 "befdc1f5a8789ae75f441062324220d7589b2e1e7071b4370ef09901e5fbd04e"
   head "https://github.com/brpc/brpc.git"
   version "0.9.git20180926"
 
+  depends_on "cmake" => :build
+  depends_on "boost"
   depends_on "leveldb"
   depends_on "protobuf"
   depends_on "glog"
@@ -16,11 +18,7 @@ class Brpc < Formula
   depends_on :macos => :sierra
 
   def install
-    mkdir "bulid" do
-      system "cmake", "..", "-DWITH_THRIFT=1", "-DWITH_GLOG=1", *std_cmake_args
-      system "make", "install"
-    end
+    system "cmake", ".", "-DWITH_THRIFT=1", "-DWITH_GLOG=1", *std_cmake_args
+    system "make", "install"
   end
 end
-
-
