53e29c46bd08ea8de94a9e0cc26bc4eecdfe8d04
Wei Mingzhi <weimingzhi@baidu.com>
2018-09-27 17:15:43 +0800

    Update zydis.rb

--- Formula/zydis.rb
+++ Formula/zydis.rb
@@ -4,7 +4,7 @@ class Zydis < Formula
   url "https://github.com/zyantific/zydis/archive/v2.0.2.tar.gz"
   sha256 "bd711102a5a30096562a7cb60bafbc9c4a2441ce5463a59f4d16f2dd73f9fb72"
   head "https://github.com/zyantific/zydis.git"
-  version "0.2.2"
+  version "2.0.2"
 
   depends_on "cmake" => :build
   depends_on :macos => :sierra
